7f05c8c73b084db45656a6f067712a5ec0159b12
Haob2110011 <nhathaotad01@gmail.com>
2024-11-25 14:06:48 +0700

    First Commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+import imageRouter from './routes/imageRoute.js';
+
+//app config
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
+
+// init middlewares
+app.use(express.json());
+app.use(cors());
+
+// api routes
+app.get('/', (req, res) => res.send('API WORKING'));
+app.use('/api/user', userRouter);
+app.use('/api/image', imageRouter);
+
+app.listen(PORT, () => console.log('Server Running on port ' + PORT));
